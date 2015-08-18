4c2ca0ae02e343aed281cdef235239dc528374dc
alex <AlexV348@gmail.com>
2015-08-18 14:45:49 -0700

    changed alerts on attempt page to be jquery modals

--- course/js/attempt_problem_set.js
+++ course/js/attempt_problem_set.js
@@ -8,38 +8,51 @@ $(document).ready(function(){
       $(this).attr("num-files",numFiles+1);
     }
   });
-});
 
-$('#soln_form').submit(function () {
-  var returnval = true
-  //varibale for all the files being renamed
-  var force_renames = "WARNING!!!!!!\n"
-  $(this).find(":file").each(function(index, element){
-    //checking for not uploaded files
-    if (element.value == '') {
-      alert('You forgot to upload a file!');
-      returnval = false;
-      //this resturn statement makes the function stop after one unsubmitted file
-      return false;
-    }
-    //check if this file is gonna be renamed and alert the user
-    else if (element.getAttribute("force-rename") == "True") {
-      var label = $("label[for='"+$(this).attr('id')+"']");
-      if (label.text() != element.value){
-        force_renames += element.value + " will be renamed to " + label.text() + "\n"
-        
+
+  $('#submitbtn').click(function () {
+    var returnval = true
+    //varibale for all the files being renamed
+    var force_renames = "";
+    $(this).parent("form").find(":file").each(function(index, element){
+      //checking for not uploaded files
+      if (element.value == '') {
+        $('#fileCheck').modal()
+        returnval = false;
+        //this resturn statement makes the function stop after one unsubmitted file
+        return false;
+      }
+      //check if this file is gonna be renamed and alert the user
+      else if (element.getAttribute("force-rename") == "True") {
+        var label = $("label[for='"+$(this).attr('id')+"']");
+        if (label.text() != element.value){
+          force_renames += "<p>" + element.value + " will be renamed to " + label.text() + "</p>";
+          
+        }
+      }
+      
+    });
+    //if all the fields are full
+    if (returnval) {
+      //if some files have different names
+      if (force_renames != ""){
+        force_renames += "<p>Check how these files are referenced!</p>";
+        $('#fileNameBody').html(force_renames);
+        $('#fileNameCheck').modal();
+        returnval = false;
       }
     }
-    
-  });
-  //if all the fields are full
-  if (returnval) {
-    //if some files have different names
-    if (force_renames != "WARNING!!!!!!\n"){
-      force_renames += "Check how these files are referenced!\nDo you still want to submit?"
-      returnval = confirm(force_renames)
+    //if returnval is true, nothing went wrong and you can submit
+    if (returnval){
+      $(this).parents("form").submit();
     }
-  }
-  //and this return statement makes the form not submit
-  return returnval
+    //else make the modal submit this form
+    var form = $(this).parents("form");
+    $("#modalSubmit").click(function(){
+      form.submit();
+    });
+    
+    return true
+  });
+
 });
