6b6f92d6a3bfbcc0ec5065ede3afdd2491c514c8
Alex Grant <AlexV348@gmail.com>
2015-08-25 16:47:35 -0700

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#used for starting gunicorn on the server
+
+BASE_DIR=`dirname $0`
+VENV_DIR=$BASE_DIR/../py_env
+
+source $VENV_DIR/bin/activate
+
+$VENV_DIR/bin/gunicorn --bind cshw.reed.edu:8000
